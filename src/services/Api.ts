565d9f7d194ddeb34468e36cad453306323d4a30
Maharshi patel <maharship0792@gmail.com>
2025-09-10 17:58:46 +0530

    set home redirection

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -31,7 +31,7 @@ api.interceptors.response.use(
     if (error.response?.status === 401) {
       localStorage.removeItem('token');
       localStorage.removeItem('user');
-      window.location.href = '/signin';
+      window.location.href = '/#/signin';
     }
     return Promise.reject(error);
   }
